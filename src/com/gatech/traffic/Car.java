734994fdafe86200adcf501e97dac689dbfaf77b
Ziyi Jiang <zjiang42@gatech.edu>
2015-01-30 14:01:45 -0500

    implement car class

--- src/com/gatech/traffic/Car.java
+++ src/com/gatech/traffic/Car.java
@@ -1,5 +1,31 @@
 package com.gatech.traffic;
 
 public class Car {
-
+	private int id;
+	private int start;
+	private int end;
+	private int speed;
+	
+	public Car(int id, int start, int end, int speed) {
+		this.id = id;
+		this.start = start;
+		this.end = end;
+		this.speed = speed;
+	}
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public int getStart(){
+		return this.start;
+	}
+	
+	public int getEnd(){
+		return this.end;
+	} 
+	
+	public int getSpeed(){
+		return this.speed;
+	}
 }
