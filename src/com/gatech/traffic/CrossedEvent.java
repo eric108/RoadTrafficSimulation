c2ff420423796b4ba7410ced048d7de217349ca0
Ziyi Jiang <zjiang42@gatech.edu>
2015-02-13 14:11:42 -0500

    fix the bug at intersection 4, fails to trigger next crossed event , thus not until next light change, no car will pass the road

--- src/com/gatech/traffic/CrossedEvent.java
+++ src/com/gatech/traffic/CrossedEvent.java
@@ -18,16 +18,17 @@ public class CrossedEvent extends AbstractEvent{
 			car.count = ++SimData.total;
 			SimData.res.add(car);
 			SimData.runwayFree[index] = true;
-			return;
-		}
-		SimData.queue.add(new ArrivalEvent(SimData.now+SimData.sectionTravelTime[index], index+1, car));
-		System.out.println("Time: " + SimData.now + " Event " + this.getClass().getSimpleName() +
+			System.out.println("Time: " + SimData.now + " Event " + this.getClass().getSimpleName() + " for Car "+car.id+" at intersection 4");
+		} else {
+			SimData.queue.add(new ArrivalEvent(SimData.now+SimData.sectionTravelTime[index], index+1, car));
+			System.out.println("Time: " + SimData.now + " Event " + this.getClass().getSimpleName() +
 				": ArrivalEvent scheduled for Car " + car.id + " at intersection " + index);
+		}
 		if(SimData.trafficLights[index] != 'R' 
 				&& ( index==4 ||SimData.intersections.get(index+1).size()<SimData.capacities[index+1] )
 				) {
 			SimData.queue.add(new CrossedEvent(SimData.now+1, index));
-			System.out.println("Time: "+ SimData.now+" Event "+this.getClass().getSimpleName() + ": CrossedEvent scheduled for Car at intersection " + index);
+			System.out.println("Time: "+ SimData.now+" Event "+this.getClass().getSimpleName() + ": CrossedEvent scheduled for Car "+ car.id+" at intersection " + index);
 		}else {
 			SimData.runwayFree[index] = true;
 		}
